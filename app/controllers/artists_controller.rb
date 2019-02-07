560b8127032c6a212dd6edfa1ded87c22f43dd49
Elise Phan <aliceconnic@gmail.com>
2019-02-07 00:55:26 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,13 @@
 class ArtistsController < ApplicationController
+ 
+  def create
+    @artist = Artist.new(artist_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+  
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
+  
 end
