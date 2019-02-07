e5cf79a107f85c65a414906e6fef170aae16bd32
Elise Phan <aliceconnic@gmail.com>
2019-02-07 01:11:29 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,9 +12,15 @@ class ArtistsController < ApplicationController
   
   def edit 
     @artist = Artist.find(params[:id])
+  end
+  
+  def update
+    @artist = Artist.find(params[:id])
     @artist.update(artist_params(:name,:bio))
     redirect_to artist_path(@artist)
   end
+  
+  def 
   
   private
   def artist_params(*args)
