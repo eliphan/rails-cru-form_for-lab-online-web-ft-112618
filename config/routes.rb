a124d119891ca89d5b6e62d6e7046be252df105e
Elise Phan <aliceconnic@gmail.com>
2019-02-07 00:29:44 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :songs, only: [:index,:new,:create:show]
   resources :genres, only: [:index,:new,:create,:show]
   resources :artists, only: [:index,:new,:create.:show]
-  get '/artists/:id', to 'artist#show'
+  get '/artists/:id/edit', to 'artists#edit' as :edit_artist
+  patch '/artists/:id', to 'aritsts#update'
+  
 end
